4a64868b1076d580db72e0e2eb7ec0581e4e0ee6
JH9 <jahyunnine@gmail.com>
2023-01-27 21:02:22 +0900

    [ADD] #12 포커 게임 서버 프로젝트 세팅

---
+++ backend/poker-service/src/main/java/com/pokerservice/PokerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pokerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerServiceApplication.class, args);
+	}
+
+}
