c22e10db519e6b970e27b6e9aac0fe61c1a92adf
Wise-eun <67956564+Wise-eun@users.noreply.github.com>
2023-02-06 19:53:55 +0900

    [Fix] 채팅 내용 버그 수정
    기존에는 플레이어가 채팅내용을 작성하면 채팅창 스크롤이 그대로 있어 사용하기에 불편함이 있었습니다.
    
    이를 고쳐, 플레이어 본인이 채팅창에 채팅을 작성해서 보내면, 바로 최하단부로 채팅창이 자동이동하게됩니다.
    
    반대로 다른 플레이어가 채팅창에 채팅을 작성할 경우에는 채팅창에 전달하되, 현재 플레이어가 보고있는 채팅창 시점에서 벗어나지 않게 하고 스크롤이 짧아지고 채팅창 내용이 조금 올라가게됩니다.

--- client/NoPOKER/Assets/Script/2D/Chatting/Chatting.cs
+++ client/NoPOKER/Assets/Script/2D/Chatting/Chatting.cs
@@ -24,7 +24,7 @@ public class Chatting : MonoBehaviour
    public void SendChatting(UICellData chattingData)
     {
         uIRecycleViewControllerSample.AddData(chattingData);
-        uIRecycleViewControllerSample.UpdataData();
+        uIRecycleViewControllerSample.UpdataMyData();
 
     }
 }
