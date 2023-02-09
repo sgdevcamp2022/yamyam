05333274c07ad4840bb2bc667003e3908efe1092
Wise-eun <67956564+Wise-eun@users.noreply.github.com>
2023-02-10 00:45:56 +0900

    [Add] WebSocket-sharp 파일 추가
    웹소켓통신을 위한 dll파일을 추가하였습니다.

--- client/NoPOKER/Assets/Script/2D/Match/Match.cs
+++ client/NoPOKER/Assets/Script/2D/Match/Match.cs
@@ -1,12 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Threading;
+using System;
+
+
 
 public class Match : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _loadingObject = new List<GameObject>();
     private IEnumerator _loadingCoroutine;
     private IEnumerator _loadingUICoroutine;
+
+    public WebSocket _socket = null;
+
+    private void Start()
+    {
+       // var wssv = new WebSocketServer("3.36.131.0", 8080);
+        _socket = new WebSocket("ws://3.36.131.0:8080/match-ws");
+        _socket.OnMessage += ws_OnMessage; //서버에서 유니티 쪽으로 메세지가 올 경우 실행할 함수를 등록한다.
+        _socket.OnOpen += ws_OnOpen;//서버가 연결된 경우 실행할 함수를 등록한다
+        _socket.OnClose += ws_OnClose;//서버가 닫힌 경우 실행할 함수를 등록한다.
+        _socket.Connect();//서버에 연결한다.
+        _socket.Send("hello");//서버에게 "hello"라는 메세지를 보낸다.
+    
+    }
+    void ws_OnMessage(object sender, MessageEventArgs e)
+    {
+        Debug.Log(e.Data);//받은 메세지를 디버그 콘솔에 출력한다.
+    }
+    void ws_OnOpen(object sender, System.EventArgs e)
+    {
+        Debug.Log("open"); //디버그 콘솔에 "open"이라고 찍는다.
+    }
+    void ws_OnClose(object sender, CloseEventArgs e)
+    {
+        Debug.Log("close"); //디버그 콘솔에 "close"이라고 찍는다.
+    }
+
+
 
     public void MatchLoading()
     {
@@ -16,8 +50,58 @@ public class Match : MonoBehaviour
         }
         _loadingCoroutine = Loading();
         _loadingUICoroutine = LoadingUI();
-
+        //Connect();
+        _socket.Send("hello");
         StartCoroutine(_loadingCoroutine);
+       
+    }
+    public void DisconnectSever()
+    {
+        try
+        {
+            if (_socket == null)
+                return;
+
+            if (_socket.IsAlive)
+                _socket.Close();
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    public void Connect()
+    {
+        try
+        {
+            if (_socket == null )
+            {
+                _socket.Connect();
+                Debug.Log("connect!");
+            }
+            else
+            {
+                if (_socket == null)
+                    Debug.Log("it is null");
+
+            }
+                
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    private void CloseConnect(object sender, CloseEventArgs e)
+    {
+        DisconnectSever();
+    }
+    public void Recv(object sender, MessageEventArgs e)
+    {
+        Debug.Log(e.Data);
+        Debug.Log(e.RawData);
     }
 
     public IEnumerator LoadingUI()
