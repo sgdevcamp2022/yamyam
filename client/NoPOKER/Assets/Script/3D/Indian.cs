1c995bbe81d27c4e05965f332aa68a36edc51d3b
Wise-eun <hee1871@naver.com>
2023-02-10 15:47:20 +0900

    [Fix] 클래스 이름 변경
    같은 이름의 클래스가 존재해 변경하였습니다.

--- client/NoPOKER/Assets/Script/3D/Indian.cs
+++ client/NoPOKER/Assets/Script/3D/Indian.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Indian : MonoBehaviour
 {
     public float speed;
     public GameObject[] weapons;
