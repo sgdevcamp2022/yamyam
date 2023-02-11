62e1d1b93016e7efb368e708c54cac64a63ebed2
pderer <capospi9903@naver.com>
2023-02-11 23:20:41 +0900

    [Add] User, Room 모델 추가

--- backend/chat_server/chatting/chat/models.py
+++ backend/chat_server/chatting/chat/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+from django.utils import timezone
+
+
+class User(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    nickname = models.CharField(max_length=150)
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=150)
+    online = models.ManyToManyField(to=User, blank=True, related_name='rooms')
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f'{self.name} ({self.get_online_count()})'
