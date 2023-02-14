31f77f7831d378eec8b435192c7dc73460e854f8
Tempnixk <chm49@naver.com>
2023-02-15 03:40:48 +0900

    [Fix] #38 방 내 최대 플레이어 수 변경
    최대 플레이어 수가 2로 설정이 되어있었습니다.

--- client/NoPOKER/Assets/Script/Common/NetworkManager.cs
+++ client/NoPOKER/Assets/Script/Common/NetworkManager.cs
@@ -10,9 +10,9 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public Text StatusText;
     public InputField roomInput, NickNameInput;
 
-    void Awake() => Screen.SetResolution(960, 540, false);
+    void Awake() => Screen.SetResolution(960, 540, false); // PC에서 빌드를 하기 위해 가로 960, 세로 540으로 했다?
 
-    void Update() => StatusText.text = PhotonNetwork.NetworkClientState.ToString();
+    void Update() => StatusText.text = PhotonNetwork.NetworkClientState.ToString(); //상태 텍스트에 어떤 상태로 연결되어있는지 표시 ( 연결되어있는지, 방에있는지, 로비에있는지)
 
     public void Connect() => PhotonNetwork.ConnectUsingSettings();
 
@@ -26,9 +26,9 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public override void OnDisconnected(DisconnectCause cause) => print("연결끊김");
     public void JoinLobby() => PhotonNetwork.JoinLobby();
     public override void OnJoinedLobby() => print("로비접속완료");
-    public void CreateRoom() => PhotonNetwork.CreateRoom(roomInput.text, new RoomOptions { MaxPlayers = 2 });
+    public void CreateRoom() => PhotonNetwork.CreateRoom(roomInput.text, new RoomOptions { MaxPlayers = 4 });
     public void JoinRoom() => PhotonNetwork.JoinRoom(roomInput.text);
-    public void JoinOrCreateRoom() => PhotonNetwork.JoinOrCreateRoom(roomInput.text, new RoomOptions { MaxPlayers = 2 }, null);
+    public void JoinOrCreateRoom() => PhotonNetwork.JoinOrCreateRoom(roomInput.text, new RoomOptions { MaxPlayers = 4 }, null);
     public void JoinRandomRoom() => PhotonNetwork.JoinRandomRoom();
     public void LeaveRoom() => PhotonNetwork.LeaveRoom();
     public override void OnCreatedRoom() => print("방만들기완료");
