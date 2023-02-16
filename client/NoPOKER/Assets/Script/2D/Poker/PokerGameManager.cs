8a6bbe269b0f10912cf2b39c041951bdd60c9c09
Wise-eun <hee1871@naver.com>
2023-02-16 12:51:44 +0900

    [Feat] 포커게임 순서 배치 및 로직 구현
    - 포커게임에서 유저 순서를 데이터로 받았을 때, 플레이어를 기준으로 한 다른유저들의 위치를 배치하였습니다.
    - 포커게임에서 배치된 순서가 실제 게임 플레이어순서대로 진행 되는게 보이도록 하였습니다.

--- client/NoPOKER/Assets/Script/2D/Poker/PokerGameManager.cs
+++ client/NoPOKER/Assets/Script/2D/Poker/PokerGameManager.cs
@@ -11,12 +11,14 @@ public enum PokerState
 
 public class PokerPlayer
 {
-    string _name;
+    public int _id;
+    public string _name;
     int _chip;
     PokerState _state = PokerState.batting;
 
-    public PokerPlayer(string name, int chip)
+    public PokerPlayer(int id, string name, int chip)
     {
+        _id = id;
         _name = name;
         _chip = chip;
     }
@@ -41,6 +43,17 @@ public class PokerPlayer
 
 }
 
+public class PokerOrder {
+    public int PokerUserID;
+    public string PokerUserNickName;
+
+    public PokerOrder(int id, string name)
+    {
+        PokerUserID = id;
+        PokerUserNickName = name;
+    }
+}
+
 public class PokerGameManager : MonoBehaviour
 {
     private static PokerGameManager s_instance = null;
@@ -57,14 +70,18 @@ public class PokerGameManager : MonoBehaviour
     private List<string> _playerNames = new List<string>();
     public float TurnTime { get => _turnTiem; }
     private int _order = 4;
-    public int NowTurn { get => _order % PeopleNum; }
+    public int NowTurn { get => _order % _peopleNum; }
     private int _gameProcessNum = 0;
     private int _distributeNum = 0;
     public int DistributeNum { get => _distributeNum; }
     private int _dieNum = 0;
     private bool _pokerFinish = false;
     public bool PokerFinish { get => _pokerFinish; }
-
+    [SerializeField] UIPokerPlayer _uiPokerPlayer;
+    private List<PokerOrder> _pokerOrder = new List<PokerOrder>();
+    private int _myOrder;
+    private int _myID = 3;
+    private int _startPoint;
 
     public void UpDieNum()
     {
@@ -84,7 +101,7 @@ public class PokerGameManager : MonoBehaviour
     private void Start()
     {
         SettingGame();
-        _turn.StartTurn(_order% _peopleNum);
+        _turn.StartTurn(NowTurn);
     }
 
 
@@ -96,15 +113,19 @@ public class PokerGameManager : MonoBehaviour
 
     public void SettingGame()
     {
+        /*
         _playerNames.Add("냠냠냠냠냠냠");
         _playerNames.Add("얌얌");
-        _playerNames.Add("쩝쩝");
+        _playerNames.Add("스구식탁");
         _playerNames.Add("뇸뇸");
 
         for (int i = 0; i < PeopleNum; i++)
         {
             PlayerOrder.Add(new PokerPlayer(_playerNames[i],90));
-        }         
+        }    */
+        GetPlayerOrder();
+        SetPlayerPosition();
+
         Batting.Instance.SettingRoundBatting(_peopleNum * 10);
 
         _card.DistributeCard();
@@ -115,6 +136,15 @@ public class PokerGameManager : MonoBehaviour
     {
 
     }*/
+
+       public void GetPlayerOrder() //플레이어 순서 가져오기
+    {
+        //test : 4명이라는 가정
+        PlayerOrder.Add(new PokerPlayer(1, "냠냠냠냠",90));
+        PlayerOrder.Add(new PokerPlayer(2, "뇸뇸",90));
+        PlayerOrder.Add(new PokerPlayer(3, "욤욤쓰",90));
+        PlayerOrder.Add(new PokerPlayer(4, "얌얌얌얌",90));
+    }
 
     public void FinishTurn()
     {
@@ -131,15 +161,13 @@ public class PokerGameManager : MonoBehaviour
             _isBattingFinish = true;
             _turn.ClearTurnUI();
             Debug.Log("Finish Turn");
-            if ((_order + 1) % _peopleNum == 0)
+            _order++;
+            if (NowTurn == _startPoint-1)
             {
                 _gameProcessNum++;
                 Debug.Log("횟수 늘어남. 현재 : " + _gameProcessNum);
             }
-            _order++;
 
-
-            Debug.Log("In else" );
             if (_gameProcessNum < 3)
             {
                 //서버통신 : 턴이 끝났다고 알림. 
@@ -200,7 +228,67 @@ public class PokerGameManager : MonoBehaviour
         //Start();
     }
 
+    public void SetPlayerPosition()
+    {
+        /*  로직
+         *  1. 받아온 순서에서 내 이름이 어디있는지 찾고, 순서(ex. 0)를 기억하기. , _playerNames.Add(내이름)
+            2. 내 숫자+1부터 (1~) 사람수까지 도는  for문을 시작.
+            3. 그 사람들을 _playerNames.Add 함.
+            4. 순서 0부터 내 숫자까지 for문을 돌림.
+            5. 그 사람들을 _playerNames.Add 함.
+            그럼, 나는0부터 자리를 잡고, 나머지 사람들은 그뒤로 쭉쭉.
+            이렇게 자리배치를 하도록함.
+            그러면 총 2개의 리스트가 생김.
+            한개는, 게임 플레이어 순서  PokerOrder
+            다른 한개는 , 나를 중심으로 한 배치순서. playerNames
+         */
+        FindMyName();
+        if (_myOrder != _peopleNum - 1) //내가 마지막 순서가 아니라면
+        {
+            for (int i = _myOrder + 1; i < _peopleNum; i++)
+            {
+                _playerNames.Add(PlayerOrder[i]._name);
+            }
+        }
+        for (int i =0; i < _myOrder; i++)
+        {
+            if(i==0)
+            {
+                if(_myOrder != 0)
+                _playerNames.Add(PlayerOrder[i]._name);
 
+                _startPoint = _playerNames.Count;
+            }
+            else
+            {
+                _playerNames.Add(PlayerOrder[i]._name);
+            }
+            
+        }
+        _order = (_peopleNum + _startPoint - 1);
+
+
+        for (int i = 0; i < _playerNames.Count; i++)
+        {
+            Debug.Log("order " + i + " : " + _playerNames[i]);
+            _uiPokerPlayer.SetUserName(i, _playerNames[i]);
+        }
+
+    }
+
+    public void FindMyName()
+    {
+        for(int i=0;i<_peopleNum;i++)
+        {
+            if(PlayerOrder[i]._id == _myID)
+            {
+                Debug.Log("my name is " + PlayerOrder[i]._name);
+                _myOrder = i;
+                _playerNames.Add(PlayerOrder[i]._name);
+                return;
+            }
+        }
+    }
 
     public void Change3DGame()
     {
@@ -210,7 +298,7 @@ public class PokerGameManager : MonoBehaviour
 
     public void NextTurn()
     { 
-        _turn.StartTurn(_order % _peopleNum);
+        _turn.StartTurn(NowTurn);
     }
 
     public void ChangePlayerState(PokerState state)
