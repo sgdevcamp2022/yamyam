1127ab810d46351c7bf2400c969529becb2d6406
Tempnixk <chm49@naver.com>
2023-02-18 14:45:06 +0900

    [Feat] 원거리 공격 구현
    원거리 공격에 필요한 발사, 재장전, 탄피 배출, 마우스 방향 피격을 구현하였습니다.

---
+++ client/NoPOKER/Assets/Script/3D/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            Destroy(gameObject, 3);
+        }
+        else if(collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
